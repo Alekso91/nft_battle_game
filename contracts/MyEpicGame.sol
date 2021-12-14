db1007e16f8158c97122a9976302754512b5a08f
Alex <alex.quilloux@student-cs.fr>
2021-12-14 17:57:47 +0100

    character SC

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -6,8 +6,36 @@ import "hardhat/console.sol";
 
 contract MyEpicGame {
 
-  
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
+  struct CharacterAttributes {
+    uint256 characterIndex;
+    uint256 hp;
+    uint256 maxHp;
+    uint256 attackDamage;
+    string name;
+    string imageURI;
+  }
+
+  CharacterAttributes[] defaultCharacters;
+
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint256[] memory characterHp,
+    uint256[] memory characterAttackDmg
+  )
+  {
+    for (uint i = 0; i < characterNames.length; i += 1) {
+      defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i]
+      }));
+
+      CharacterAttributes memory c = defaultCharacters[i];
+      console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+    }
   }
 }
