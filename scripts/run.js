04302c61ebc252651f5dcc54ce9bf911a51bc4a5
Alex <alex.quilloux@student-cs.fr>
2021-12-14 18:07:42 +0100

    run characters

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+      ["King", "Spoon", "Adventurer"],
+      ["https://i.postimg.cc/sMGYRYWd/1.png",
+      "https://i.postimg.cc/2q4n1CT0/426.png",
+      "https://i.postimg.cc/rd6SyDGd/4993.png"],
+      [700, 1000, 500],
+      [100, 200, 150]
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
   };
